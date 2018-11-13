3dcc3b7705d4cdb072f18691a46ab3366e6889f4
Zachary Lehmann <zlehmann@gmail.com>
2018-11-13 22:51:11 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,21 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+  connection = ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/school-test.db"
+  )
+
+  sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT
+    )
+  SQL
+
+  ActiveRecord::Base.connection.execute(sql)
+
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
